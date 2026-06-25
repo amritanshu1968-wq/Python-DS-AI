de77aa76318ddc72eec48e1024153d3dab5b0415
amritanshu1968-wq <amritanshu1968@gmail.com>
2026-06-25 10:30:18 +0530

    Second day

---
+++ day 2/print.py
@@ -0,0 +1,25 @@
+#method 1
+print("Hello world")
+
+#method 2
+message = "Hello world"
+print(message)
+
+#method 3
+text = "Hello world"
+print(text)
+
+#method 4
+def greet():
+    print("Hello world")
+
+#method 5
+text_1 = "hello"
+text_2 = "world"
+print(text_1 + " " + text_2)
+
+#method 6
+print("Hello", "world")
+
+#method 7
+print("Hello" + " " + "world")
