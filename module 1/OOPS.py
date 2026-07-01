4ea4684208169b48a62edc07fcfa879e2142eb96
amritanshu1968-wq <amritanshu1968@gmail.com>
2026-07-01 10:21:57 +0530

    new

--- module 1/OOPS.py
+++ module 1/OOPS.py
@@ -1,3 +1,22 @@
 #encapsulation
 
+#define a class named bank
+class Bank:
+#constructor: Automatically called when an object of the class is created
+    def __init__(self):
+
+#double underscore(__) makes it private (name)
+        self.__balance = 100000      # private attribute
+    def deposit(self, amount):
+        self.__balance += amount
+#display the current balance
+    def show_balance(self):
+        print(self.__balance)
+
+
+#calling
+b = Bank()
+b.deposit(10000)
+b.show_balance()  # Output: 110000
+
 
